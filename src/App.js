3b3f770479150fe0700fa972f65e75648957b5ea
DenKolles <den.kolles@gmail.com>
2022-11-27 02:28:16 +0500

    update dependencies (#13)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import type {Node} from 'react';
-import {View, Button} from 'react-native';
+import {View} from 'react-native';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 import {RootNavigator} from './navigators/RootNavigator';
 import {NavigationContainer} from '@react-navigation/native';
 
 import auth from '@react-native-firebase/auth';
-import {GoogleSignin} from '@react-native-community/google-signin';
+import {GoogleSignin, GoogleSigninButton, statusCodes,} from '@react-native-google-signin/google-signin';
+
 
 const App: () => Node = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
 
-  GoogleSignin.configure({
-    webClientId:
-      '922360239960-j6tg82l1vp0prvio45eqdbcvhfsc7uoh.apps.googleusercontent.com',
-  });
+  
 
   async function onGoogleButtonPress() {
     // Get the user's ID token
@@ -37,13 +35,15 @@ const App: () => Node = () => {
           alignItems: 'center',
           justifyContent: 'center',
         }}>
-        <Button
-          title="Google Sign-In"
+        <GoogleSigninButton
+          style={{ width: 192, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
           onPress={() =>
             onGoogleButtonPress().then(() =>
               console.log('Signed in with Google!'),
             )
-          }
+          }   
         />
       </View>
     );
@@ -57,6 +57,10 @@ const App: () => Node = () => {
   }
 
   useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '922360239960-j6tg82l1vp0prvio45eqdbcvhfsc7uoh.apps.googleusercontent.com',
+    });
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
